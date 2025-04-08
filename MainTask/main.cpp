ad9568d8f5f98fe9383cdc05c00e5f4c3e8b61dc
Arseniy Shablyko <241208arseniy@gmail.com>
2025-04-08 18:47:08 +0300

    Test

--- MainTask/main.cpp
+++ MainTask/main.cpp
@@ -1,28 +1,33 @@
 #include "Students.h"
 
 int main() {
-	Student st1, st2;
+	Student* st1 = nullptr;
+	Student* st2 = nullptr;
 
-	st1.name = "Veronika";
-	st1.age = 14;
-	st1.mark = 9.2;
-	st1.alive = true;
+	st1 = new Student;
 
-	st2.name = "Danik";
-	st2.age = 15;
-	st2.mark = 9.99;
-	st2.alive = false;
+	st1->name = "Veronika";
+	st1->age = 14;
+	st1->mark = 9.2;
+	st1->alive = true;
+	   
+	/*st2->name = "Danik";
+	st2->age = 15;
+	st2->mark = 9.99;
+	st2->alive = false;*/
+
+	st2 = st1;
 
 	cout << "Before: " << endl;
-	cout << st1.getString();
-	cout << st2.getString();
+	cout << st1->getString();
+	cout << st2->getString();
 
-	st1 = st2;
-	st1.name = "Bogdan";
+	/*st1 = st2;*/
+	st1->name = "Bogdan";
 
 	cout << "After: " << endl;
-	cout << st1.getString();
-	cout << st2.getString();
+	cout << st1->getString();
+	cout << st2->getString();
 
 	return 0;
 }
